b80465f3f28155768a16a122d97f3f281acef3a8
ada-wh525 <wenzhi.hu25@ic.ac.uk>
2025-10-15 05:31:21 +0100

    idiot blank

--- tests/test_simple_functions.py
+++ tests/test_simple_functions.py
@@ -1,26 +1,25 @@
 import pytest
-
 from simple_functions import my_sum, factorial
 
 
-class TestSimpleFunctions(object):
-    '''Class to test our simple functions are working correctly'''
+class TestSimpleFunctions:
+    """Class to test our simple functions are working correctly"""
 
     @pytest.mark.parametrize('iterable, expected', [
         ([8, 7, 5], 20),
-        ((10, -2, 5, -10, 1), 4)
+        ((10, -2, 5, -10, 1), 4),
     ])
     def test_my_add(self, iterable, expected):
-        '''Test our add function'''
+        """Test our add function"""
         isum = my_sum(iterable)
         assert isum == expected
+
     @pytest.mark.parametrize('number, expected', [
         (5, 120),
         (3, 6),
-        (1, 1)
+        (1, 1),
     ])
     def test_factorial(self, number, expected):
-        '''Test our factorial function'''
+        """Test our factorial function"""
         answer = factorial(number)
         assert answer == expected
-
