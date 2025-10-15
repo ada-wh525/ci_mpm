981b0c4a83035185b57369f8df95a3e0f87d4cd4
ada-wh525 <wenzhi.hu25@ic.ac.uk>
2025-10-15 04:36:24 +0100

    add the functions

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,5 +1,5 @@
 
-__all__ = ['my_sum']
+__all__ = ['my_sum', 'factorial']
 
 
 def my_sum(iterable):
@@ -7,3 +7,8 @@ def my_sum(iterable):
     for i in iterable:
         tot += i
     return tot
+
+from functools import cache   # Py 3.9+ 提供
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n else 1
